1dcc99f13c9de71770b7508198d357294ffb190c
Tomer Parizer <tomerpar133@gmail.com>
2017-07-31 02:09:32 +0300

    added open camera options to AddOrUpdate

--- app/src/main/java/com/example/shaysheli/androaid_final/fragments/AddOrEditFragment.java
+++ app/src/main/java/com/example/shaysheli/androaid_final/fragments/AddOrEditFragment.java
@@ -5,7 +5,10 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,6 +24,7 @@ import com.example.shaysheli.androaid_final.Model.Movie;
 import com.example.shaysheli.androaid_final.Model.MyDatePicker;
 import com.example.shaysheli.androaid_final.R;
 
+import static android.app.Activity.RESULT_OK;
 import static com.example.shaysheli.androaid_final.fragments.MovieDetailFragment.tran;
 /**
  * A simple {@link Fragment} subclass.
@@ -151,7 +155,32 @@ public class AddOrEditFragment extends Fragment implements View.OnClickListener{
 
         btnAddEdit.setOnClickListener(this);
 
+        edtImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
         return v;
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            edtImage.setImageBitmap(imageBitmap);
+        }
     }
 
     @Override
